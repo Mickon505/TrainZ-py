311c33a9402aad115864a39494c6a324dbcb1bbd
dedko <=>
2021-08-03 12:30:11 +0200

    some changes

--- scripts/ResolutionPicker.py
+++ scripts/ResolutionPicker.py
@@ -1,42 +1,58 @@
-from tkinter import Tk, StringVar, OptionMenu, Button
+from tkinter import Tk, StringVar, OptionMenu, Button, Label
 run = True
 
-def window(min_res, max_res):
-	"""
-	min_res = minimum resolution to choose from
-	max_res = maximum resolution to choose from
-	"""
 
-	root = Tk()
-	root.title("Select Resolution")
-	#  root.geometry('700x500')
-	 
+class ResolutionPicker:
+	def __init__(self):
+		self.run = True
 
-	resolutions_list = ["640 × 360",
-						"720 × 480",
-						"800 × 600",
-						"1024 × 768"
-						"1280 × 720",
-						"1366 ×	768",
-						"1600 ×	900",
-						"1920 × 1080"]
+	def window(self, min_res=0, max_res=0):
+		"""
+		min_res = minimum resolution to choose from
+		max_res = maximum resolution to choose from
+		"""
 
-	value_inside = StringVar(root)
-	
-	value_inside.set("Select an Option")
-	
-	question_menu = OptionMenu(root, value_inside, *options_list)
-	question_menu.pack()
-	  
-	submit_button = Button(root, text='Submit', command=exit)
-	submit_button.pack()
-	  
-	if run:
-		root.mainloop()
+		root = Tk()
+		root.title("Select Resolution")
+		#  root.geometry('700x500')
 
-	else:
-		return value_inside.get()
+		resolutions_list = ["640 × 360",
+							"720 × 480",
+							"800 × 600",
+							"1024 × 768",
+							"1280 × 720",
+							"1366 × 768",
+							"1600 × 900",
+							"1920 × 1080"]
+
+		value_inside = StringVar(root)
+		
+		value_inside.set("Select an Option")
+
+		label = Label(root, text="Select Screen Resolution")
+		label.pack()
+		
+		question_menu = OptionMenu(root, value_inside, *resolutions_list)
+		question_menu.pack()
+
+		submit_button = Button(root, text='Submit', command=self.exit)
+		submit_button.pack()
+
+		print(self.run)
+
+		if run:
+			root.mainloop()
+
+		else:
+			return value_inside.get()
 
 
-def exit():
-	run = False
+	// TODO returnúť value_inside a destroynúť res window 
+
+	def exit(self):
+		self.run = False
+
+
+if __name__ == '__main__':
+	win = ResolutionPicker().window()
+	print(win)
